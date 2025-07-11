0858c6ac619aef3c60fb8fb19191109da0753e96
DaniloCastroCE <38533079+DaniloCastroCE@users.noreply.github.com>
2025-07-11 01:01:06 -0300

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -39,7 +39,8 @@ router.get("/getUser", checkAuth, async (req, res) => {
   try {
     const relatorio =  await Relatorio.findOne({temp_id: req.session.user._id_temp})
     dados_relatorio = {
-      relatorio.dia_plantao, relatorio.nome_plantao
+      dia_plantao: relatorio.dia_plantao, 
+      nome_plantao: relatorio.nome_plantao
     }
     
   }catch(err){
